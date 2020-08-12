33543d240f28d5e7dbddfe7e7e6ee00d6ed31e46
vandercoder <andikawilliam@outlook.com>
2020-08-12 14:55:11 +0700

    initial commit

---
+++ content.js
@@ -0,0 +1,39 @@
+let products = [
+  [
+    "/resources/rails-twitter/Homepage.png",
+    "Hand Sanitizer",
+    "Hand Sanitizer",
+    "60 mL",
+    "https://github.com/vandercoder/micro-twitter",
+    "https://shrouded-temple-93524.herokuapp.com"
+  ],
+  [
+    "/resources/rails-eventmaster/Home.png",
+    "Hand Sanitizer",
+    "Hand Sanitizer",
+    "60 mL",
+    "",
+  ],
+  [
+    "/resources/rails-flight_booker/Home.png",
+    "Hand Sanitizer",
+    "Hand Sanitizer",
+    "100 mL",
+    "",
+  ],
+  [
+    "/resources/rails-flight_booker/Home.png",
+    "Hand Sanitizer",
+    "Hand Sanitizer",
+    "100 mL",
+    "",
+  ],
+  [
+    "/resources/rails-flight_booker/Home.png",
+    "Hand Sanitizer",
+    "Hand Sanitizer",
+    "100 mL",
+    "",
+  ]
+];
+
