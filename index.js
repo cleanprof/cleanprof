5f64240e447886d6eb80b12d3658eda020b0ba7e
cleanprof <cleanprofid@gmail.com>
2020-08-13 15:13:15 +0700

    Added Vue.js, used list rendering for products

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
-let techStacks = [products];
-let projectCards = document.querySelector("#project-cards");
-
-let template = document.querySelector("#card-template");
-
 $(".navburger").click(function() {
   $(".navmenu").toggleClass("relative");
   $(".navmenu").toggleClass("hidden");
 });
 
-insertProjects(techStacks[0]);
-
-function insertProjects(stackProjects) {
-  let div = document.createElement("div");
-  $(div).addClass("container mx-auto md:flex md:justify-center")
-  $(div).hide()
-
-  stackProjects.forEach(function(projectContent) {
-    let card = template.content.cloneNode(true);
-    let cardMain = card.querySelector(".card-main");
-  
-    let img = card.querySelector("img");
-    img.setAttribute("src", projectContent[0]);
-    img.setAttribute("alt", projectContent[1]);
-    
-    let cardTitle = card.querySelector(".card-title");
-    cardTitle.textContent = projectContent[2];
-    
-    let cardContent = card.querySelector(".card-content");
-    cardContent.textContent = projectContent[3];
-    
-    let githubLink = card.querySelector(".price");
-    githubLink.setAttribute("href", projectContent[4])
-
-    $(div).append(card);
-  });
-  $(projectCards).append(div);
-  $(div).fadeIn(600);
-
-}
+var projectCards = new Vue({
+  el: '#project-cards',
+  data: {
+    products : [
+      {
+        id : 1,
+        image : "../resources/product-display.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "60 mL",
+        price : "IDR 70,000",
+        link : "https://www.tokopedia.com/"
+      },
+      {
+        id : 2,
+        image : "../resources/product-display.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "60 mL",
+        price : "IDR 70,000",
+        link : "https://www.tokopedia.com/",
+      },
+      { id : 3, 
+        image : "../resources/product-display.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "IDR 70,000",
+        link : "https://www.tokopedia.com/",
+      },
+      { id : 4,
+        image : "../resources/product-display.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "IDR 70,000",
+        link : "https://www.tokopedia.com/",
+      },
+      { id : 5,
+        image : "../resources/product-display.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "IDR 70,000",
+        link : "https://www.tokopedia.com/",
+      }
+    ]    
+  }
+})
