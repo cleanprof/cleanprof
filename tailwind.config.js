30e4744e980fae6c0fe88f372a674c6a3c2dfb20
cleanprof <cleanprofid@gmail.com>
2020-08-14 16:35:27 +0700

    add working mobile sidenav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,27 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      spacing: {
+        '5/100': '5%',
+        '7/100': '7%',
+        '1/10': '10%',
+        '1/5': '20%',
+        '4/5screen': '80vh', 
+      },
+      height: {
+        'logo': '10%',
+      },
+      minHeight: {
+        '4/5screen': '80vh',
+      },
       colors: {
         'orange-bright': '#FCB01C',
         'green-grass': '#679966',
         'white-green': '#FFFEF1',
       },
       borderRadius: {
-        'body-info': '50px',
+        'desktop-info': '3rem',
+        'mobile-info': '2rem',
         'footer-tl': '100% 120%',
         'footer-tr': '40% 85%',
       }
