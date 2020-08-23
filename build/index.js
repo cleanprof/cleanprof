d60f667f8dfc5742d2600ed66f5ce211f14887d1
cleanprof <cleanprofid@gmail.com>
2020-08-23 13:03:45 +0700

    added fade animation to dark layer when opening sidebar

--- build/index.js
+++ build/index.js
@@ -1,22 +1,29 @@
 let htmlElement = document.querySelector('html');
 let bodyElement = document.querySelector('body');
-let overlayElement = document.querySelector('.overlay-black');
+let overlayElement = document.querySelector('#overlay');
 
-document.getElementById("navburger").addEventListener("click", function(){
+function disableScroll() {
   htmlElement.style.overflow = 'hidden';
   bodyElement.style.overflow = 'hidden';
-  overlayElement.style.display = 'block';
-  overlayElement.style.opacity = 0.5;
+}
 
-  document.getElementById("navmenu").style.width = "75%";
+function enableScroll() {
+  htmlElement.style.overflow = '';
+  bodyElement.style.overflow = '';
+}
+
+document.getElementById("navburger").addEventListener("click", function(){
+  // disableScroll();
+  overlayElement.classList.add("visible");
+
+  document.getElementById("navmenu").style.width = "60%";
 });
 
 document.getElementById("navclose").addEventListener("click", function(){
+  // enableScroll();
+  overlayElement.classList.remove("visible");
+
   document.getElementById("navmenu").style.width = "0";
-  htmlElement.style.overflow = '';
-  bodyElement.style.overflow = '';
-  overlayElement.style.opacity = 0;
-  overlayElement.style.display = 'hidden';
 });
 
 var projectCards = new Vue({
