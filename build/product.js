406559364a3695c4008a65272e0432f9426767f5
cleanprof <cleanprofid@gmail.com>
2020-09-02 20:50:28 +0700

    updated images

--- build/product.js
+++ build/product.js
@@ -4,7 +4,7 @@ var projectCards = new Vue({
     products : [
       {
         id : 1,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/dis-1L-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "60 mL",
@@ -14,7 +14,7 @@ var projectCards = new Vue({
       },
       {
         id : 2,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/dis-5L-nu-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "60 mL",
@@ -24,7 +24,7 @@ var projectCards = new Vue({
       },
       { 
         id : 3, 
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/dis-250-aerosol-belakang-nu-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "100 mL",
@@ -34,7 +34,7 @@ var projectCards = new Vue({
       },
       { 
         id : 4,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/dis-250-aerosol-nu-low pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "100 mL",
@@ -44,7 +44,7 @@ var projectCards = new Vue({
       },
       { 
         id : 5,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/dis-250-belakang-nu-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "100 mL",
@@ -54,7 +54,7 @@ var projectCards = new Vue({
       },
       {
         id : 6,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/dis-250-nu-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "60 mL",
@@ -64,7 +64,7 @@ var projectCards = new Vue({
       },
       {
         id : 7,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/hs-1L-nu-lox-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "60 mL",
@@ -74,7 +74,7 @@ var projectCards = new Vue({
       },
       { 
         id : 8, 
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/hs-5L-nu-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "100 mL",
@@ -84,7 +84,7 @@ var projectCards = new Vue({
       },
       { 
         id : 9,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/hs-60-belakang-nu-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "100 mL",
@@ -94,14 +94,75 @@ var projectCards = new Vue({
       },
       { 
         id : 10,
-        image : "./resources/product-display.png",
+        image : "./resources/product-pic/hs-60-nu-low-pix.png",
         imageAlt : "Hand Sanitizer",
         name : "Hand Sanitizer",
         spec : "100 mL",
         price : "99,999",
         discountPrice : "",
         link : "http://www.tokopedia.com/cleanprofid",
-      }
+      },
+      { 
+        id : 11,
+        image : "./resources/product-pic/hs-100-belakang-nu-low-pix.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "99,999",
+        discountPrice : "",
+        link : "http://www.tokopedia.com/cleanprofid",
+      },
+      { 
+        id : 12,
+        image : "./resources/product-pic/hs-100-nu-low-pix.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "99,999",
+        discountPrice : "",
+        link : "http://www.tokopedia.com/cleanprofid",
+      },
+      { 
+        id : 13,
+        image : "./resources/product-pic/hs-250-belakang-nu-low-pix.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "99,999",
+        discountPrice : "",
+        link : "http://www.tokopedia.com/cleanprofid",
+      },
+      { 
+        id : 14,
+        image : "./resources/product-pic/hs-250-nu-low-pix.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "99,999",
+        discountPrice : "",
+        link : "http://www.tokopedia.com/cleanprofid",
+      },
+      { 
+        id : 15,
+        image : "./resources/product-pic/hs-500-pump-nu-low-pix.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "99,999",
+        discountPrice : "",
+        link : "http://www.tokopedia.com/cleanprofid",
+      },
+      { 
+        id : 16,
+        image : "./resources/product-pic/hs-500-pump-transparan-nu-low-pix.png",
+        imageAlt : "Hand Sanitizer",
+        name : "Hand Sanitizer",
+        spec : "100 mL",
+        price : "99,999",
+        discountPrice : "",
+        link : "http://www.tokopedia.com/cleanprofid",
+      },
+      
     ]    
   }
 })
