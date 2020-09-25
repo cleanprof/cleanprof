728601313901b4c2a0ed7aea15996e22ff96d9e7
vandercoder <andikawilliam@outlook.com>
2020-09-25 20:08:36 +0700

    separate component vue form index js, fix errors

--- build/js/index.js
+++ build/js/index.js
@@ -26,24 +26,3 @@ navClose.addEventListener("click", function(){
   overlayElement.classList.remove("visible");
   document.getElementById("navmenu").style.width = "0";
 });
-
-
-var products = new Vue({
-  el: '#products',
-  data: {
-    products : productList  
-  },
-  methods: {
-    discountPrice: function (product) {
-      discountPercent = +((product.discount).split('%')[0]);
-      finalPrice = +(product.price) * (100-discountPercent)/100;
-      return this.priceCommaString(finalPrice);
-    },
-
-    priceCommaString: function (price) {
-      var parts = price.toString().split(".");
-      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      return parts.join(".");
-    }
-  }
-})
