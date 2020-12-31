612f6a6e3b60622f63e7ed5b019e6c94ee5ae239
vandercoder <andikawilliam@outlook.com>
2020-12-31 14:01:41 +0700

    deploy

--- js/index.js
+++ js/index.js
@@ -16,31 +16,16 @@ gsap.fromTo(
   }
 )
 
-let htmlElement = document.querySelector('html');
-let bodyElement = document.querySelector('body');
 let overlayElement = document.querySelector('#overlay');
-
-function disableScroll() {
-  htmlElement.style.overflow = 'hidden';
-  bodyElement.style.overflow = 'hidden';
-}
-
-function enableScroll() {
-  htmlElement.style.overflow = '';
-  bodyElement.style.overflow = '';
-}
-
 let navBurger = document.getElementById("navburger");
 let navClose = document.getElementById("navclose");
 
 navBurger.addEventListener("click", function(){
-  // disableScroll();
   overlayElement.classList.add("visible");
   document.getElementById("navmenu").style.width = "60%";
 });
 
 navClose.addEventListener("click", function(){
-  // enableScroll();
   overlayElement.classList.remove("visible");
   document.getElementById("navmenu").style.width = "0";
 });
